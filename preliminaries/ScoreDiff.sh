747a04985b26851bf2f6715c7423712df3afa76b
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-10 09:40:34 -0700

    removed test results and updated test script

--- preliminaries/ScoreDiff.sh
+++ preliminaries/ScoreDiff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python simple.py bwv66.6.mxl test.mxl
+python simple.py bwv66.6.mxl different_pitches.mxl
 python simple.py bwv66.6.mxl different_key.mxl
 python simple.py bwv66.6.mxl different_time.mxl
 

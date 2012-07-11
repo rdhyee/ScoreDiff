1b09e8cd0e9d58a1c4449984f592420df2b3483a
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-11 13:07:22 -0700

    added some small comments

--- preliminaries/simple.py
+++ preliminaries/simple.py
@@ -1,6 +1,7 @@
 from music21 import *
 from sys import argv
 
+#doctests
 def test1(score1, score2):
 	"""
 	   >>> test1('bwv66.6.mxl', 'different_pitches.mxl')
@@ -30,13 +31,13 @@ def test1(score1, score2):
 
 
 
-
+	#for now, I'm only testing the first part and the first measure
 	if (parsed1.parts[0].measure(0).keySignature.pitchAndMode==parsed2.parts[0].measure(0).keySignature.pitchAndMode):
 		print "Both scores start in the same key signature"
 	else:
 		print "The two scores do not start in the same key"
 
-
+	#again, only testing the first part and first measure
 	if (parsed1.parts[0].measure(0).timeSignature.numerator==parsed2.parts[0].measure(0).timeSignature.numerator and parsed1.parts[0].measure(0).timeSignature.denominator==parsed2.parts[0].measure(0).timeSignature.denominator):
 		print "The scores have the same time signature"
 
@@ -45,7 +46,7 @@ def test1(score1, score2):
 
 	parsed1_pitches=parsed1.parts[0].pitches
 	parsed2_pitches=parsed2.parts[0].pitches
-
+	#this will check the entire piece, but only for part one
 	for index, pitch in enumerate(parsed1_pitches):
 		if not (parsed2_pitches[index]==pitch):
 			print "Found a pitch that was not the same"

c5552583a0aea432890d8c61ac64102c863332f5
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-11 12:46:42 -0700

    changed the testing to use automatic doctesting. Cleaner approach

--- preliminaries/simple.py
+++ preliminaries/simple.py
@@ -1,46 +1,61 @@
 from music21 import *
 from sys import argv
 
-us=environment.UserSettings()
-us['localCorpusPath']='.'
-score1=argv[1]
-score2=argv[2]
-
-parsed1=corpus.parse(score1)
-parsed2=corpus.parse(score2)
-
-print "Analyzing "+score1+" and "+score2
-print "Checking to see if the scores are well-formed..."
-
-if(parsed1.isWellFormedNotation() and parsed2.isWellFormedNotation()):
-	print "Both scores are well formed"
-else:
-	print "At least one of the input scores is not well formed."
-
-print "Comparing Key Signatures..."
-
-if (parsed1.parts[0].measure(0).keySignature.pitchAndMode==parsed2.parts[0].measure(0).keySignature.pitchAndMode):
-	print "Both scores start in the same key signature"
-else:
-	print "The two scores do not start in the same key"
-
-print "Comparing Time Signatures..."
-
-if (parsed1.parts[0].measure(0).timeSignature.numerator==parsed2.parts[0].measure(0).timeSignature.numerator and parsed1.parts[0].measure(0).timeSignature.denominator==parsed2.parts[0].measure(0).timeSignature.denominator):
-	print "The scores have the same time signature (at least for first measure)"
-
-else:
-	print "The scores do not start in the same time signature"
-
-print "Comparing pitches..."
-parsed1_pitches=parsed1.parts[0].pitches
-parsed2_pitches=parsed2.parts[0].pitches
-
-for index, pitch in enumerate(parsed1_pitches):
-	if not (parsed2_pitches[index]==pitch):
-		print "Found a pitch that was not the same"
+def test1(score1, score2):
+	"""
+	   >>> test1('bwv66.6.mxl', 'different_pitches.mxl')
+	   Both scores start in the same key signature
+	   The scores have the same time signature
+	   Found a pitch that was not the same
+	   Found a pitch that was not the same
+	   Found a pitch that was not the same
+	   Found a pitch that was not the same
+		
+	   >>> test1('bwv66.6.mxl','different_key.mxl')
+	   The two scores do not start in the same key
+	   The scores have the same time signature
 
 
-print "Finished comparing scores"
-print "Goodbye!"
+	   >>> test1('bwv66.6.mxl', 'different_time.mxl')
+	   Both scores start in the same key signature
+	   The scores do not start in the same time signature
+	   
 
+
+
+	"""
+	
+	us=environment.UserSettings()
+	us['localCorpusPath']='.'
+
+	parsed1=corpus.parse(score1)
+	parsed2=corpus.parse(score2)
+
+
+
+
+	if (parsed1.parts[0].measure(0).keySignature.pitchAndMode==parsed2.parts[0].measure(0).keySignature.pitchAndMode):
+		print "Both scores start in the same key signature"
+	else:
+		print "The two scores do not start in the same key"
+
+
+	if (parsed1.parts[0].measure(0).timeSignature.numerator==parsed2.parts[0].measure(0).timeSignature.numerator and parsed1.parts[0].measure(0).timeSignature.denominator==parsed2.parts[0].measure(0).timeSignature.denominator):
+		print "The scores have the same time signature"
+
+	else:
+		print "The scores do not start in the same time signature"
+
+	parsed1_pitches=parsed1.parts[0].pitches
+	parsed2_pitches=parsed2.parts[0].pitches
+
+	for index, pitch in enumerate(parsed1_pitches):
+		if not (parsed2_pitches[index]==pitch):
+			print "Found a pitch that was not the same"
+
+
+
+
+if __name__=='__main__':
+	import doctest
+	doctest.testmod()

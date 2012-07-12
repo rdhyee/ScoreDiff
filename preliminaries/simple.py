e340fea3e693f43be8381850f030514c2053d437
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-11 23:22:46 -0700

    changed organization

--- preliminaries/simple.py
+++ preliminaries/simple.py
@@ -3,7 +3,7 @@ from sys import argv
 
 
 us=environment.UserSettings()
-us['localCorpusPath']='.'
+us['localCorpusPath']='./test_cases'
 
 
 #compares the starting key signature
@@ -79,6 +79,150 @@ def check_pitches(score1, score2):
 	
 	return parsed1_pitches==parsed2_pitches
 
+#compares the clef markings in score1 against those in score2
+def check_clef(score1, score2):
+
+	"""
+	   >>> check_clef('bwv66.6.mxl','different_clef.mxl')
+	   The scores don't start with the same clef sign
+
+	   >>> check_clef('bwv66.6.mxl','different_accidentals')
+	   Both scores start with the same clef sign
+
+	   >>> check_clef('bwv66.6.mxl','different_stems.mxl')
+	   Both scores start with the same clef sign
+
+
+	"""
+	
+	parsed1=corpus.parse(score1)
+	parsed2=corpus.parse(score2)
+
+
+
+	if(parsed1.parts[0].measure(0).clef.sign==parsed2.parts[0].measure(0).clef.sign):
+		print "Both scores start with the same clef sign"
+	else:
+		print "The scores don't start with the same clef sign"
+
+#compares the accidentals in score1 against those in score2
+def check_accidentals(score1, score2):
+
+	"""
+	   >>> check_accidentals('bwv66.6.mxl','different_accidentals')
+	   found an accidental in one version that is not in the other version
+	   found an accidental in one version that is not in the other version
+
+	   >>> check_accidentals('bwv66.6.mxl','different_clef.mxl')
+           The accidentals are the same
+
+	   >>> check_accidentals('bwv66.6.mxl','different_stems.mxl')
+	   The accidentals are the same
+
+	"""
+	parsed1=corpus.parse(score1)
+	parsed2=corpus.parse(score2)
+	
+	notes1=parsed1.parts[0].measure(0).notes
+	notes2=parsed2.parts[0].measure(0).notes
+
+
+	test=False
+	for index in range(0,len(notes1)):
+	
+		if(notes1[index].pitch.accidental is None and not (notes2[index].pitch.accidental is None)):
+			print "found an accidental in one version that is not in the other version"
+			test=True
+		
+       
+		elif(notes2[index].pitch.accidental is None and not(notes1[index].pitch.accidental is None)):
+			print "found an accidental in one version that is not in the other version"
+			test=True
+		
+	
+		elif(notes1[index].pitch.accidental is None and notes2[index].pitch.accidental is None):
+			continue	
+	
+		elif(notes1[index].pitch.accidental.fullName != notes2[index].pitch.accidental.fullName):
+			print "found an accidental in one version that is not in the other version"
+			test=True
+
+	if(not test):
+		print "The accidentals are the same"
+
+#compares the stem directions in score1 against those in score2
+def check_stems(score1, score2):
+	
+	"""
+	   >>> check_stems('bwv66.6.mxl', 'different_stems.mxl')
+	   found different stem directions
+	   found different stem directions
+
+	   >>> check_stems('bwv66.6.mxl','different_accidentals.mxl')
+	   The stem directions are the same
+
+	   >>> check_stems('bwv66.6.mxl','different_clef.xml')
+	   The stem directions are the same
+
+	"""
+
+	parsed1= corpus.parse(score1)
+	parsed2= corpus.parse(score2)
+
+	notes1= parsed1.parts[0].measure(0).notes
+	notes2= parsed2.parts[0].measure(0).notes
+
+	test=False
+	for index in range(0,len(notes1)):
+		if(notes1[index].stemDirection != notes2[index].stemDirection):
+			print "found different stem directions"
+			test=True
+	if(not test):
+		print "The stem directions are the same"
+
+#compare the ornaments in score1 against those in score2
+def check_ornaments(score1, score2):
+
+	"""
+	   >>> check_ornaments('bwv66.6.mxl','different_ornaments.mxl')
+	   different ornaments
+	   different ornaments
+
+	   >>> check_ornaments('bwv66.6.mxl', 'bwv66.6.mxl')
+	   The ornaments are the same
+
+	"""
+
+	parsed1=corpus.parse(score1)
+	parsed2=corpus.parse(score2)
+
+	notes1=parsed1.parts[0].measure(0).notes
+	notes2=parsed2.parts[0].measure(0).notes
+
+	test=False
+
+	for index, item in enumerate(notes1):
+
+		if (item.expressions==[] and notes2[index].expressions!=[] 
+				or notes2[index].expressions==[] and item.expressions!=[]):
+			
+			print "different ornaments"
+			test=True
+
+		elif (item.expressions==[] and notes2[index].expressions==[]):
+
+			continue
+		
+		elif (item.expressions[0].classes!=notes2[index].expressions[0].classes):
+			
+			print "different ornaments"
+			test=True
+	
+	if(not test):
+		print "The ornaments are the same"
+
+#compare the dynamics in score1 against those in score2
+def check_dynamics(score1, score2):
 
 
 if __name__=='__main__':
---
+++ preliminaries/test_cases/test.py
@@ -0,0 +1,13 @@
+
+from music21 import *
+from sys import argv
+
+score=argv[1]
+
+us=environment.UserSettings()
+us['localCorpusPath']='.'
+
+s=corpus.parse(score)
+s.show()
+exit()
+

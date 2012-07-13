b941aa3fe6d9146908019a4a146e0078767b5218
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-12 20:50:40 -0700

    changed function names, and output

--- preliminaries/simple.py
+++ preliminaries/simple.py
@@ -7,16 +7,17 @@ us['localCorpusPath']='./test_cases'
 #compares the starting key signature
 #of score1 against the starting
 #time signature of score2
-def check_key(score1, score2):
+def have_same_key_signature(score1, score2):
 	"""
-	   >>> check_key('bwv66.6.mxl','different_key.mxl')
-	   The two scores do not start in the same key
+	   >>> have_same_key_signature('bwv66.6.mxl','different_key.mxl')
+	   False
 
-	   >>> check_key('bwv66.6.mxl','different_pitches.mxl')
-	   Both scores start in the same key signature
+	   >>> have_same_key_signature('bwv66.6.mxl','different_pitches.mxl')
+	   True
 
-	   >>> check_key('bwv66.6.mxl','different_time.mxl')
-	   Both scores start in the same key signature
+	   >>> have_same_key_signature('bwv66.6.mxl','different_time.mxl')
+	   True
+
 	"""
 	
 
@@ -27,45 +28,45 @@ def check_key(score1, score2):
 
 	#for now, I'm only testing the first part and the first measure
 	if (parsed1.parts[0].measure(0).keySignature.pitchAndMode==parsed2.parts[0].measure(0).keySignature.pitchAndMode):
-		print "Both scores start in the same key signature"
+		return True
 	else:
-		print "The two scores do not start in the same key"
+		return False	
 
 #compares the starting time signature of score1 against the
 #starting time signature of score2
-def check_time(score1, score2):
+def have_same_time_signature(score1, score2):
 	
 	"""
-	   >>> check_time('bwv66.6.mxl','different_time.mxl')
-	   The scores do not start in the same time signature
-	   
-	   >>> check_time('bwv66.6.mxl','different_pitches.mxl')
-	   The scores have the same time signature
+	   >>> have_same_time_signature('bwv66.6.mxl','different_time.mxl')
+	   False
 
-	   >>> check_time('bwv66.6.mxl','different_key.mxl')
-	   The scores have the same time signature
+	   >>> have_same_time_signature('bwv66.6.mxl','different_pitches.mxl')
+	   True
+
+	   >>> have_same_time_signature('bwv66.6.mxl','different_key.mxl')
+	   True
 
 	"""
 	parsed1=corpus.parse(score1)
 	parsed2=corpus.parse(score2)
 	#again, only testing the first part and first measure
 	if (parsed1.parts[0].measure(0).timeSignature.numerator==parsed2.parts[0].measure(0).timeSignature.numerator and parsed1.parts[0].measure(0).timeSignature.denominator==parsed2.parts[0].measure(0).timeSignature.denominator):
-		print "The scores have the same time signature"
+		return True
 
 	else:
-		print "The scores do not start in the same time signature"
+		return False
 
 #compares the pitches in score1 against the pitches in score2
-def check_pitches(score1, score2):
+def have_same_pitches(score1, score2):
 
 	"""
-	   >>> check_pitches('bwv66.6.mxl','different_pitches.mxl')
+	   >>> have_same_pitches('bwv66.6.mxl','different_pitches.mxl')
 	   False
 
-	   >>> check_pitches('bwv66.6.mxl','different_key.mxl')
+	   >>> have_same_pitches('bwv66.6.mxl','different_key.mxl')
 	   True
 
-	   >>> check_pitches('bwv66.6.mxl','different_time.mxl')
+	   >>> have_same_pitches('bwv66.6.mxl','different_time.mxl')
 	   True
 
 	"""
@@ -78,17 +79,17 @@ def check_pitches(score1, score2):
 	return parsed1_pitches==parsed2_pitches
 
 #compares the clef markings in score1 against those in score2
-def check_clef(score1, score2):
+def have_same_clef_markings(score1, score2):
 
 	"""
-	   >>> check_clef('bwv66.6.mxl','different_clef.mxl')
-	   The scores don't start with the same clef sign
+	   >>> have_same_clef_markings('bwv66.6.mxl','different_clef.mxl')
+	   False
 
-	   >>> check_clef('bwv66.6.mxl','different_accidentals')
-	   Both scores start with the same clef sign
+	   >>> have_same_clef_markings('bwv66.6.mxl','different_accidentals')
+	   True
 
-	   >>> check_clef('bwv66.6.mxl','different_stems.mxl')
-	   Both scores start with the same clef sign
+	   >>> have_same_clef_markings('bwv66.6.mxl','different_stems.mxl')
+	   True
 
 
 	"""
@@ -99,23 +100,22 @@ def check_clef(score1, score2):
 
 
 	if(parsed1.parts[0].measure(0).clef.sign==parsed2.parts[0].measure(0).clef.sign):
-		print "Both scores start with the same clef sign"
+		return True
 	else:
-		print "The scores don't start with the same clef sign"
+		return False
 
 #compares the accidentals in score1 against those in score2
-def check_accidentals(score1, score2):
+def have_same_accidentals(score1, score2):
 
 	"""
-	   >>> check_accidentals('bwv66.6.mxl','different_accidentals')
-	   found an accidental in one version that is not in the other version
-	   found an accidental in one version that is not in the other version
+	   >>> have_same_accidentals('bwv66.6.mxl','different_accidentals')
+           False
 
-	   >>> check_accidentals('bwv66.6.mxl','different_clef.mxl')
-           The accidentals are the same
+	   >>> have_same_accidentals('bwv66.6.mxl','different_clef.mxl')
+           True
 
-	   >>> check_accidentals('bwv66.6.mxl','different_stems.mxl')
-	   The accidentals are the same
+	   >>> have_same_accidentals('bwv66.6.mxl','different_stems.mxl')
+           True
 
 	"""
 	parsed1=corpus.parse(score1)
@@ -125,42 +125,37 @@ def check_accidentals(score1, score2):
 	notes2=parsed2.parts[0].measure(0).notes
 
 
-	test=False
+
 	for index in range(0,len(notes1)):
 	
 		if(notes1[index].pitch.accidental is None and not (notes2[index].pitch.accidental is None)):
-			print "found an accidental in one version that is not in the other version"
-			test=True
+			return False	
 		
        
 		elif(notes2[index].pitch.accidental is None and not(notes1[index].pitch.accidental is None)):
-			print "found an accidental in one version that is not in the other version"
-			test=True
+			return False	
 		
 	
 		elif(notes1[index].pitch.accidental is None and notes2[index].pitch.accidental is None):
 			continue	
 	
 		elif(notes1[index].pitch.accidental.fullName != notes2[index].pitch.accidental.fullName):
-			print "found an accidental in one version that is not in the other version"
-			test=True
+			return False	
 
-	if(not test):
-		print "The accidentals are the same"
+	return True
 
 #compares the stem directions in score1 against those in score2
-def check_stems(score1, score2):
+def have_same_stem_directions(score1, score2):
 	
 	"""
-	   >>> check_stems('bwv66.6.mxl', 'different_stems.mxl')
-	   found different stem directions
-	   found different stem directions
+	   >>> have_same_stem_directions('bwv66.6.mxl', 'different_stems.mxl')
+	   False
 
-	   >>> check_stems('bwv66.6.mxl','different_accidentals.mxl')
-	   The stem directions are the same
+	   >>> have_same_stem_directions('bwv66.6.mxl','different_accidentals.mxl')
+	   True
 
-	   >>> check_stems('bwv66.6.mxl','different_clef.xml')
-	   The stem directions are the same
+	   >>> have_same_stem_directions('bwv66.6.mxl','different_clef.xml')
+	   True
 
 	"""
 
@@ -170,24 +165,20 @@ def check_stems(score1, score2):
 	notes1= parsed1.parts[0].measure(0).notes
 	notes2= parsed2.parts[0].measure(0).notes
 
-	test=False
 	for index in range(0,len(notes1)):
 		if(notes1[index].stemDirection != notes2[index].stemDirection):
-			print "found different stem directions"
-			test=True
-	if(not test):
-		print "The stem directions are the same"
+			return False
+	return True
 
 #compare the ornaments in score1 against those in score2
-def check_ornaments(score1, score2):
+def have_same_ornaments(score1, score2):
 	
 	"""
-	   >>> check_ornaments('bwv66.6.mxl','different_ornaments.mxl')
-	   different ornaments
-	   different ornaments
+	   >>> have_same_ornaments('bwv66.6.mxl','different_ornaments.mxl')
+	   False
 
-	   >>> check_ornaments('bwv66.6.mxl', 'bwv66.6.mxl')
-	   The ornaments are the same
+	   >>> have_same_ornaments('bwv66.6.mxl', 'bwv66.6.mxl')
+	   True
 
 	"""
 
@@ -196,8 +187,7 @@ def check_ornaments(score1, score2):
 
 	notes1=parsed1.parts[0].measure(0).notes
 	notes2=parsed2.parts[0].measure(0).notes
-
-	test=False
+	
 	o='Ornament'
 	for index, item in enumerate(notes1):
 		
@@ -205,8 +195,7 @@ def check_ornaments(score1, score2):
 		if (item.expressions==[] and notes2[index].expressions!=[] 
 				or notes2[index].expressions==[] and item.expressions!=[]):
 			
-			print "different ornaments"
-			test=True
+			return False	
 
 		elif (item.expressions==[] and notes2[index].expressions==[]):
 
@@ -214,22 +203,17 @@ def check_ornaments(score1, score2):
 		
 		
 		elif(o in item.expressions[0].classes and not o in notes2[index].expressions[0].classes or o in notes2[index].expressions[0].classes and not o in item.expressions[0].classes):
+			return False	
 				
-			print "different ornaments"
-			test=True
-		
 		elif(not o in item.expressions[0].classes and not o in notes2[index].expressions[0].classes):
 			
 			continue
 
 		elif(item.expressions[0].classes[item.expressions[0].classes.index(o)-1]!=notes2[index].expressions[0].classes[notes2[index].expressions[0].classes.index(o)-1]):
 
-			print "different ornaments"
-			test=True
-			
-	if(not test):
-		print "The ornaments are the same"
+			return False
 
+	return True
 
 #This function was originally intended to check for
 #phrasing only, but that turned out to be a somewhat
@@ -237,17 +221,17 @@ def check_ornaments(score1, score2):
 #including slurs, glissandos, etc.  
 #Take a look at: http://mit.edu/music21/doc/html/moduleSpanner.html?highlight=spanner
 #to read more about spanners
-def check_spanners(score1, score2):
+def have_same_spanners(score1, score2):
 	
 
 	"""
-	   >>> check_spanners('bwv66.6.mxl','different_phrasing.mxl')
+	   >>> have_same_spanners('bwv66.6.mxl','different_phrasing.mxl')
 	   False
 
-	   >>> check_spanners('bwv66.6.mxl', 'different_ornaments.mxl')
+	   >>> have_same_spanners('bwv66.6.mxl', 'different_ornaments.mxl')
 	   False
 
-	   >>> check_spanners('bwv66.6.mxl', 'different_dynamics.mxl')
+	   >>> have_same_spanners('bwv66.6.mxl', 'different_dynamics.mxl')
 	   True
 
 	"""
@@ -260,16 +244,16 @@ def check_spanners(score1, score2):
 
 	return spanner1==spanner2
 
-def check_articulations(score1, score2):
+def have_same_articulations(score1, score2):
 
 	"""
-	   >>> check_articulations('bwv66.6.mxl','different_articulations.mxl')
+	   >>> have_same_articulations('bwv66.6.mxl','different_articulations.mxl')
 	   True
 
-	   >>> check_articulations('bwv66.6.mxl', 'different_phrasing.mxl')
+	   >>> have_same_articulations('bwv66.6.mxl', 'different_phrasing.mxl')
 	   False
 
-	   >>> check_articulations('bwv66.6.mxl', 'different_ornaments.mxl')
+	   >>> have_same_articulations('bwv66.6.mxl', 'different_ornaments.mxl')
 	   False
 
 	"""
@@ -288,6 +272,7 @@ def check_articulations(score1, score2):
 			return True
 	
 	return False
+
 
 if __name__=='__main__':
 	import doctest
